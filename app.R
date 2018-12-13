878a0befa118d4bc2031ff033e48be6b8a6851b8
leebunce <leebunce@hotmail.com>
2018-12-13 22:18:26 +0000

    Create basic functioning app for mtcars

--- app.R
+++ app.R
@@ -3,7 +3,11 @@
 # Packages ----------------------------------------------------------------
 library(shiny)
 library(readr)
+library(dplyr)
 library(validate)
+
+# Validation rules --------------------------------------------------------
+validator_mtcars <- validator(.file = 'Validation rules/mtcars.yaml')
 
 # UI ----------------------------------------------------------------------
 ui <- fluidPage(
@@ -23,23 +27,37 @@ ui <- fluidPage(
       ),
       
       mainPanel(
-        tableOutput("input_file")
+        tabsetPanel(type = "tabs",
+                    tabPanel("Data", tableOutput("input_file")),
+                    tabPanel("Errors", tableOutput("errors"))
+        )
       )
    )
 )
 
 # Server ------------------------------------------------------------------
 server <- function(input, output) {
-   
-  output$input_file <- renderTable({
+  
+  file <- reactive({
     
-    input_file <- input$file
-    
-    if (is.null(input_file))
+    if (is.null(input$file))
       return(NULL)
     
-    read_csv(input_file$datapath)
+    read_csv(input$file$datapath)
   })
+   
+  output$input_file <- renderTable({file()})
+  
+  output$errors <- renderTable({
+    if (is.null(file()))
+      return(NULL)
+    
+    confront(file(), validator_mtcars, key = "car") %>% 
+      as.data.frame() %>% 
+      filter(value == F) %>% 
+      arrange(car, name) %>% 
+      rename(error = name)
+    })
     
 }
 
